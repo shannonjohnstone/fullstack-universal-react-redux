117e28e4e4d1187badd0cee4256a12b75c3c13e3
shannonjohnstone <shannon@shannonjohnstone.com.au>
2017-06-04 12:49:11 +1000

    fix eslint errors

--- src/reducers/cartReducers.js
+++ src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-export function cartReducers(state = [], action) {
+export default function (state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
       return { cart: [...state, action.payload] }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 
 import booksReducers from './booksReducers'
-import { cartReducers } from './cartReducers'
+import cartReducers from './cartReducers'
 
 export default combineReducers({
   books: booksReducers,
