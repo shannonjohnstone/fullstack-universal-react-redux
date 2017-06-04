d3616ed8dd787a3a576f9d6095d65550508a1ca6
shannonjohnstone <shannon@shannonjohnstone.com.au>
2017-06-04 12:16:46 +1000

    update webpack to use es2017

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015', 'stage-1']
+          // presets: ['react', 'es2015', 'es2016', 'es2017', 'stage-1', 'stage-0']
+          presets: ['react', 'es2017', 'stage-0']
         }
       }
     ]
