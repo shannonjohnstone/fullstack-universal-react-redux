d53a07d917565d844334683a3824ddbf1407889c
shannonjohnstone <shannon@shannonjohnstone.com.au>
2017-06-04 12:48:04 +1000

    update max-len

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     "semi": [2, "never"],
     "comma-dangle": [0, "never"],
     "import/no-extraneous-dependencies": ["error", {"devDependencies": true, "optionalDependencies": false, "peerDependencies": false}],
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "max-len": ["error", 150]
   }
 };
