dcf99eacb59c79c10b08aa6dd0a07c4f2c5d08ff
shannonjohnstone <shannon@shannonjohnstone.com.au>
2017-06-06 23:41:00 +1000

    add disalbled functionality for buttons add base styling to view items on page (coming back to more detailed styling at the ned

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { string, func } from 'prop-types'
 import classNames from 'classnames'
 
-function Button({ buttonType, value, onClick }) {
+function Button({ buttonType, value, onClick, disabled }) {
   const btnClass = classNames({
     'btn--default': buttonType === 'default',
     'btn--primary': buttonType === 'primary',
     'btn--success': buttonType === 'success',
     'btn--danger': buttonType === 'danger',
   })
-  return <button className={`btn ${btnClass}`} onClick={onClick} >{value}</button>
+  return <button className={`btn ${btnClass}`} onClick={onClick} disabled={disabled}>{value}</button>
 }
 
 Button.propTypes = {
